1bf894bd07c85b5ed1ca0490fc2ed3ddf1067789
Yihui Xiong <yihui.xiong@hotmail.com>
2020-05-28 09:17:16 +0800

    add a new board - nRF52840 MDK USB Dongle

---
+++ firmware/nrf/nrf52840_mdk_usb_dongle/config/custom_board.h
@@ -0,0 +1,48 @@
+#ifndef MDK_DONGLE_H
+#define MDK_DONGLE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define LEDS_NUMBER    3
+
+#define LED_1          22
+#define LED_2          23
+#define LED_3          24
+
+#define LEDS_LIST { LED_1, LED_2, LED_3 }
+
+#define LEDS_ACTIVE_STATE 1
+
+#define BSP_LED_0      LED_1
+#define BSP_LED_1      LED_2
+#define BSP_LED_2      LED_3
+
+#define LEDS_INV_MASK  LEDS_MASK
+
+#define BUTTONS_NUMBER 2
+
+#define SW_1           4
+#define SW_2           5
+#define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
+
+#define BUTTONS_LIST { SW_1, SW_2 }
+
+#define BUTTONS_ACTIVE_STATE 0
+
+#define BSP_BUTTON_0   SW_1
+#define BSP_BUTTON_1   SW_2
+
+#define RX_PIN_NUMBER  20
+#define TX_PIN_NUMBER  19
+#define CTS_PIN_NUMBER 21
+#define RTS_PIN_NUMBER  2
+#define HWFC           false
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
